b8ed5c792b2c933a727064f319fd43c0adc0318a
Lee Zamparo <zamparo@gmail.com>
2025-02-24 22:41:17 -0500

    generation, retrieval seem to work

--- src/hypotheses/query_gemeni.py
+++ src/hypotheses/query_gemeni.py
@@ -1,12 +1,70 @@
+import os
 import google.genai as genai
 from google.genai import types
+from google.genai.types import Part
 
-client = genai.Client(api_key="")
-model_specification = "gemeni-1.5-flash"
+from typing import Optional
 
 
-# model = genai.models.get("google/gemeni-1.5-flash")
-# GOOGLE_API_KEY = ""
+def get_gemeni_client():
+    client = genai.Client()
+    return client
 
-# genai.configure(GOOGLE_API_KEY)
-# model.configure(api_key=GOOGLE_API_KEY)
+
+def get_hypothesis_instructions(file_path: str) -> types.ContentUnion:
+    """Read and package prompt instructions as
+    appropriate for Gemeni
+
+    Args:
+        file_path (str): path to prompt instruction text file
+
+    Returns:
+        types.ContentUnion: prompt instructions
+    """
+    instructions = []
+
+    with open(os.path.relpath(file_path), "r") as f:
+        for line in f.readlines():
+            if not line.strip():
+                continue
+            instructions.append(line.strip())
+    return instructions
+
+
+def create_generation_config(
+    system_instructions: Optional[list[str]] = None,
+) -> types.GenerateContentConfig:
+    config = types.GenerateContentConfig(
+        system_instruction=system_instructions,
+    )
+    return config
+
+
+def build_contents(query: list[str], context: list[str]) -> list[Part]:
+    # Context has to be formed into a single string
+    if len(context) > 1:
+        context = " ".join(context)
+    return [Part(text=context), Part(text=query)]
+
+
+def generate_response(
+    query: list[str],
+    client: genai.Client,
+    model_specification: str = "models/gemini-1.5-flash",
+    context: Optional[list[str]] = None,
+    instructions: Optional[types.ContentUnion] = None,
+):
+    config = (
+        create_generation_config(system_instructions=instructions)
+        if instructions
+        else None
+    )
+    contents = (
+        build_contents(query=query, context=context) if context else [Part(text=query)]
+    )
+    response = client.models.generate_content(
+        model=model_specification,
+        contents=contents,
+        config=config,
+    )
+    return response
---
+++ tests/test_query_gemeni.py
@@ -0,0 +1,82 @@
+import pytest
+from src.hypotheses.query_gemeni import (
+    generate_response,
+    get_gemeni_client,
+    get_hypothesis_instructions,
+)
+
+
+@pytest.fixture
+def get_hypothesis_instructions_fixture():
+    import os
+
+    print(f"we are in {os.getcwd()}")
+    return get_hypothesis_instructions(file_path="./data/hypothesis_prompt")
+
+
+@pytest.fixture
+def get_string_evidence():
+    return [
+        "{'stringId_A': '9606.ENSP00000375629', 'stringId_B': '9606.ENSP00000366024', 'preferredName_A': 'LILRB2', 'preferredName_B': 'HLA-G', 'ncbiTaxonId': 9606, 'score': 0.999, 'nscore': 0, 'fscore': 0, 'pscore': 0, 'ascore': 0, 'escore': 0.955, 'dscore': 0.5, 'tscore': 0.982}",
+        "{'stringId_A': '9606.ENSP00000375629', 'stringId_B': '9606.ENSP00000379873', 'preferredName_A': 'LILRB2', 'preferredName_B': 'HLA-A', 'ncbiTaxonId': 9606, 'score': 0.984, 'nscore': 0, 'fscore': 0, 'pscore': 0, 'ascore': 0, 'escore': 0.405, 'dscore': 0.5, 'tscore': 0.953}",
+        "{'stringId_A': '9606.ENSP00000375629', 'stringId_B': '9606.ENSP00000259951', 'preferredName_A': 'LILRB2', 'preferredName_B': 'HLA-F', 'ncbiTaxonId': 9606, 'score': 0.983, 'nscore': 0, 'fscore': 0, 'pscore': 0, 'ascore': 0, 'escore': 0.549, 'dscore': 0.5, 'tscore': 0.933}",
+        "{'stringId_A': '9606.ENSP00000375629', 'stringId_B': '9606.ENSP00000399168', 'preferredName_A': 'LILRB2', 'preferredName_B': 'HLA-B', 'ncbiTaxonId': 9606, 'score': 0.981, 'nscore': 0, 'fscore': 0, 'pscore': 0, 'ascore': 0, 'escore': 0.549, 'dscore': 0.5, 'tscore': 0.925}",
+        "{'stringId_A': '9606.ENSP00000375629', 'stringId_B': '9606.ENSP00000497910', 'preferredName_A': 'LILRB2', 'preferredName_B': 'B2M', 'ncbiTaxonId': 9606, 'score': 0.979, 'nscore': 0, 'fscore': 0, 'pscore': 0, 'ascore': 0, 'escore': 0.96, 'dscore': 0.5, 'tscore': 0}",
+        "{'stringId_A': '9606.ENSP00000375629', 'stringId_B': '9606.ENSP00000362524', 'preferredName_A': 'LILRB2', 'preferredName_B': 'ANGPTL2', 'ncbiTaxonId': 9606, 'score': 0.963, 'nscore': 0, 'fscore': 0, 'pscore': 0, 'ascore': 0, 'escore': 0.483, 'dscore': 0, 'tscore': 0.932}",
+        "{'stringId_A': '9606.ENSP00000375629', 'stringId_B': '9606.ENSP00000391592', 'preferredName_A': 'LILRB2', 'preferredName_B': 'PTPN6', 'ncbiTaxonId': 9606, 'score': 0.943, 'nscore': 0, 'fscore': 0, 'pscore': 0, 'ascore': 0, 'escore': 0.457, 'dscore': 0.9, 'tscore': 0}",
+        "{'stringId_A': '9606.ENSP00000375629', 'stringId_B': '9606.ENSP00000364898', 'preferredName_A': 'LILRB2', 'preferredName_B': 'SYK', 'ncbiTaxonId': 9606, 'score': 0.9, 'nscore': 0, 'fscore': 0, 'pscore': 0, 'ascore': 0, 'escore': 0, 'dscore': 0.9, 'tscore': 0}",
+        "{'stringId_A': '9606.ENSP00000375629', 'stringId_B': '9606.ENSP00000365402', 'preferredName_A': 'LILRB2', 'preferredName_B': 'HLA-C', 'ncbiTaxonId': 9606, 'score': 0.89, 'nscore': 0, 'fscore': 0, 'pscore': 0, 'ascore': 0, 'escore': 0.549, 'dscore': 0.5, 'tscore': 0.555}",
+        "{'stringId_A': '9606.ENSP00000375629', 'stringId_B': '9606.ENSP00000359345', 'preferredName_A': 'LILRB2', 'preferredName_B': 'RPL5', 'ncbiTaxonId': 9606, 'score': 0.755, 'nscore': 0, 'fscore': 0, 'pscore': 0, 'ascore': 0, 'escore': 0, 'dscore': 0, 'tscore': 0.755}",
+    ]
+
+
+@pytest.fixture
+def get_positive_string_test():
+    return (
+        "HLA-G likely interacts physically with LILRB2",
+        "{'stringId_A': '9606.ENSP00000375629', 'stringId_B': '9606.ENSP00000366024', 'preferredName_A': 'LILRB2', 'preferredName_B': 'HLA-G', 'ncbiTaxonId': 9606, 'score': 0.999, 'nscore': 0, 'fscore': 0, 'pscore': 0, 'ascore': 0, 'escore': 0.955, 'dscore': 0.5, 'tscore': 0.982}",
+    )
+
+
+@pytest.fixture
+def get_negative_string_test():
+    return (
+        "HLA-F likely interacts physically with LILRB2",
+        [
+            "{'stringId_A': '9606.ENSP00000375629', 'stringId_B': '9606.ENSP00000366024', 'preferredName_A': 'LILRB2', 'preferredName_B': 'HLA-G', 'ncbiTaxonId': 9606, 'score': 0.4, 'nscore': 0, 'fscore': 0, 'pscore': 0, 'ascore': 0, 'escore': 0.3, 'dscore': 0.1, 'tscore': 0.212}",
+            "{'stringId_A': '9606.ENSP00000375629', 'stringId_B': '9606.ENSP00000379873', 'preferredName_A': 'LILRB2', 'preferredName_B': 'HLA-A', 'ncbiTaxonId': 9606, 'score': 0.3, 'nscore': 0, 'fscore': 0, 'pscore': 0, 'ascore': 0, 'escore': 025, 'dscore': 0.1, 'tscore': 0.222}",
+            "{'stringId_A': '9606.ENSP00000375629', 'stringId_B': '9606.ENSP00000259951', 'preferredName_A': 'LILRB2', 'preferredName_B': 'HLA-F', 'ncbiTaxonId': 9606, 'score': 0.1, 'nscore': 0, 'fscore': 0, 'pscore': 0, 'ascore': 0, 'escore': 0.1, 'dscore': 0.03, 'tscore': 0.111}",
+        ],
+    )
+
+
+## test below here
+
+
+def test_positive_string(get_positive_string_test):
+    query, context = get_positive_string_test
+    client = get_gemeni_client()
+    instructions = [
+        "please provide an answer that begis with yes or no, with justification that follows"
+    ]
+    response = generate_response(
+        query=query, client=client, context=context, instructions=instructions
+    )
+    assert ("Yes" in response.text) or ("yes" in response.text)
+
+
+def test_negative_string(get_negative_string_test):
+    query, context = get_negative_string_test
+    client = get_gemeni_client()
+    instructions = [
+        "please provide an answer that begis with yes or no, with justification that follows"
+    ]
+    response = generate_response(
+        query=query, client=client, context=context, instructions=instructions
+    )
+    assert ("No" in response.text) or ("no" in response.text)
+
+
+def test_retrieve_detailed_instructions(get_hypothesis_instructions_fixture):
+    instructions = get_hypothesis_instructions_fixture
+    assert len(instructions) > 0
